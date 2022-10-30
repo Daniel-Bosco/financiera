413daee7aeaad176bb82b3085aae5621c0536905
Daniel <sergio.bosco@uap.edu.ar>
2022-10-30 10:30:37 -0300

    agregando gema chartkick

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :capacities
+  resources :sectors
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'chart', to: 'capacities#chart'
+
+  root "sectors#index"
 end
